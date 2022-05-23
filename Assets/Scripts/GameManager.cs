7f66ab44390cdd33493c0dfb32f5f7514d3d8424
Tristan <cailbourdint@gmail.com>
2022-05-23 15:37:59 +0200

    Added UI for lives and score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 namespace BrickBreaker
 {
@@ -17,15 +19,51 @@ namespace BrickBreaker
         private PaddleController _paddle;
         [SerializeField] private Ball ballPrefab;
         [SerializeField] private int currentScore;
-        
+        [SerializeField] private int lives = 3;
+
         public Ball CurrentBall { get; private set; }
 
+        [Header("Events")]
+        public UnityEvent<int> onScoreChanged;
+        public UnityEvent<int> onLivesChanged;
+
         private void Start()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            FindPaddle();
+            InitializeLivesUi();
+        }
+        
+        /// <summary>
+        /// Called when a new scene is loaded
+        /// </summary>
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            FindPaddle();
+            InitializeLivesUi();
+        }
+
+        /// <summary>
+        /// Find the paddle and spawn the ball
+        /// </summary>
+        private void FindPaddle()
         {
             _paddle = FindObjectOfType<PaddleController>();
             if (_paddle != null)
             {
                 SpawnBall();
+            }
+        }
+
+        /// <summary>
+        /// Initialize the lives UI
+        /// </summary>
+        private void InitializeLivesUi()
+        {
+            var gameUi = FindObjectOfType<GameUi>();
+            if (gameUi != null)
+            {
+                gameUi.UpdateLives(lives);
             }
         }
 
@@ -38,17 +76,29 @@ namespace BrickBreaker
             CurrentBall = Instantiate(ballPrefab, spawnPos, ballPrefab.transform.rotation);
             CurrentBall.SetPaddle(_paddle);
             
-            CurrentBall.onBallFallBellowPaddle.AddListener(DestroyBall);
+            CurrentBall.onBallFallBellowPaddle.AddListener(OnBallFall);
         }
 
         /// <summary>
         /// Called when the ball falls bellow the paddle
         /// </summary>
-        private void DestroyBall()
+        private void OnBallFall()
         {
             if (CurrentBall == null) return;
             Destroy(CurrentBall.gameObject);
-            SpawnBall();
+            
+            // Update the current lives count
+            lives--;
+            onLivesChanged?.Invoke(lives);
+
+            if (lives <= 0)
+            {
+                //TODO: Show game over screen
+            }
+            else
+            {
+                SpawnBall();
+            }
         }
 
         /// <summary>
@@ -57,6 +107,12 @@ namespace BrickBreaker
         public void AddScore(int value)
         {
             currentScore += value;
+            onScoreChanged?.Invoke(currentScore);
+        }
+
+        private void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
         }
     }
 }
---
+++ Assets/Scripts/GameUi.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace BrickBreaker
+{
+    public class GameUi : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text livesText;
+        [SerializeField] private TMP_Text currentScoreText;
+        [SerializeField] private TMP_Text highScoreText;
+
+        private void Start()
+        {
+            GameManager.Instance.onScoreChanged.AddListener(UpdateCurrentScore);
+            GameManager.Instance.onLivesChanged.AddListener(UpdateLives);
+        }
+
+        /// <summary>
+        /// Update the lives text
+        /// </summary>
+        public void UpdateLives(int lives)
+        {
+            livesText.text = $"Lives: {lives}";
+        }
+
+        /// <summary>
+        /// Update the current score text
+        /// </summary>
+        private void UpdateCurrentScore(int score)
+        {
+            currentScoreText.text = $"Score: {score}";
+        }
+
+        private void OnDestroy()
+        {
+            GameManager.Instance.onScoreChanged.RemoveListener(UpdateCurrentScore);
+            GameManager.Instance.onLivesChanged.RemoveListener(UpdateLives);
+        }
+    }
+}
